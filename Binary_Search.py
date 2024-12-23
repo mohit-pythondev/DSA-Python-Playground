f3dcb8d14b4040d1c0cda7282509320d2b564f29
MOhit <mohit.pythondev@gmail.com>
2024-12-23 23:38:32 +0530

    Binary Search add

---
+++ Binary_Search.py
@@ -0,0 +1,22 @@
+def binary_search(arr, target):
+    start = 0
+    end = len(arr) - 1
+    while start <= end:
+        mid = (start + end) // 2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            start = mid + 1
+        else:
+            end = mid - 1
+    return -1
+
+
+arr = [2, 4, 6, 8, 10]
+target = 8
+result = binary_search(arr, target)
+
+if result != -1:
+    print(f"Element {target} found at index {result}.")
+else:
+    print(f"Element {target} not found.")
--- Linear_Search.py
+++ Linear_Search.py
@@ -6,7 +6,7 @@ def linear_search(arr, target):
             return i  # Return the index of the found element
     return -1  # Return -1 if the element is not found
 
-# Example usage
+# Example usage 
 arr = [2, 4, 6, 8, 10]
 target = 8
 result = linear_search(arr, target)

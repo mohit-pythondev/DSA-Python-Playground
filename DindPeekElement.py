792a45d6412d60208c0a34821ad8ad1b85ad4556
MOhit <mohit.pythondev@gmail.com>
2024-12-29 15:36:36 +0530

    feat: add new code file for peak element search

---
+++ DindPeekElement.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def findPeakElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if nums:
+            start, end = 0, len(nums) - 1
+            while start < end:
+                mid = start + (end - start) // 2
+                if nums[mid] < nums[mid + 1]:
+                    start = mid + 1
+                else:
+                    end = mid
+            return start
+        return -1

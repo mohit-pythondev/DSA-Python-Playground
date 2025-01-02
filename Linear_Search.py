7d711fdfe775fdc0d08ed99260e44b69650a33e8
MOhit <mohit.pythondev@gmail.com>
2025-01-03 00:27:48 +0530

    Adding PPT

--- Linear_Search.py
+++ Linear_Search.py
@@ -1,10 +1,9 @@
 def linear_search(arr, target):
     # Iterate through the list
     for i in range(len(arr)):
-        # Check if the current element is the target
         if arr[i] == target:
-            return i  # Return the index of the found element
-    return -1  # Return -1 if the element is not found
+            return i  
+    return -1
 
 
     """
